425510d572c008a7ee91739bc7c28d73d06c7407
Trixesu <trixteresu@gmail.com>
2024-10-15 17:44:35 +0700

    Update fUrutanBilangan.java

--- fUrutanBilangan.java
+++ fUrutanBilangan.java
@@ -14,7 +14,25 @@ public class fUrutanBilangan {
         
         System.out.print("Masukkan Bilangan Ketiga : ");
         int bil3 = input.nextInt();
-        
+              
+        if (bil1 <= bil2 && bil2 <= bil3) {
+            System.out.println("Urutan Bilangan : "+bil1+", "+bil2+", "+bil3);
+        }
+        else if (bil1 <= bil3 && bil3 <= bil2) {
+            System.out.println("Urutan Bilangan : "+bil1+", "+bil3+", "+bil2);
+        }
+        else if (bil2 <= bil1 && bil1 <= bil3) {
+            System.out.println("Urutan Bilangan : "+bil2+", "+bil1+", "+bil3);
+        }
+        else if (bil2 <= bil3 && bil3 <= bil1) {
+            System.out.println("Urutan Bilangan : "+bil2+", "+bil3+", "+bil1);
+        }
+        else if (bil3 <= bil1 && bil1 <= bil2) {
+            System.out.println("Urutan Bilangan : "+bil3+", "+bil1+", "+bil2);
+        }
+        else {
+            System.out.println("Urutan Bilangan : "+bil3+", "+bil2+", "+bil2);
+        }
         input.close();
     }
 }
