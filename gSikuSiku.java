ac8f3c5576a5a1d0b9189b3ce1325931da073e7e
Trixesu <trixteresu@gmail.com>
2024-10-15 17:45:15 +0700

    Update gSikuSiku.java

--- gSikuSiku.java
+++ gSikuSiku.java
@@ -6,23 +6,17 @@ public class gSikuSiku {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Sisi A : ");
+        System.out.print("Sisi a : ");
         double sisiA = input.nextDouble();
         
-        System.out.print("Sisi B : ");
+        System.out.print("Sisi b : ");
         double sisiB = input.nextDouble();
         
-        System.out.print("Sisi C : ");
+        System.out.print("Sisi c (Sisi Terpanjang): ");
         double sisiC = input.nextDouble();
 
-        if (Math.pow(sisiA, 2) == (Math.pow(sisiB, 2)) + (Math.pow(sisiC, 2))) {
+        if (Math.pow(sisiC, 2) == (Math.pow(sisiA, 2)) + (Math.pow(sisiB, 2))) {
             System.out.println("Segitiga tersebut berbentuk siku-siku");
-        }
-        else if (Math.pow(sisiB, 2) == (Math.pow(sisiA, 2)) + (Math.pow(sisiC, 2))) {
-            System.out.println("Segitiga tersebut berbentuk siku-siku");
-        }
-        else if (Math.pow(sisiC, 2) == (Math.pow(sisiA, 2)) + (Math.pow(sisiB, 2))) {
-            System.out.println("Segitiga tersebut berbentuk siku-siku");        
         }
         else {
             System.out.println("Segitiga tersebut tidak berbentuk siku-siku");
